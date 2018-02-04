959296ba762536ddb5122bb6280b14f56b5cba3b
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2018-02-04 23:47:09 +0700

    Added place_ships and write_shot function

--- bot_stima.py
+++ bot_stima.py
@@ -1,3 +1,10 @@
+# File and path names
+command_file = "command.txt"
+place_ship_file = "place.txt"
+game_state_file = "state.json"
+output_path = '.'   # NOT YET SURE
+map_size = 0        # NOT YET SURE
+
 # Command converter
 commands = {
     "singleShot" : 1,
@@ -9,3 +16,30 @@ commands = {
     "seekerMissile" : 7,
     "shield" : 8
 }
+
+def place_ships():
+    " This is where the ships location determined "
+    # ship format <Shipname> <x> <y> <direction>
+    # Ship names: Battleship, Cruiser, Carrier, Destroyer, Submarine
+    # Directions: north east south west
+
+    # TO BE CONTINUED (each ships' location)
+    ships =[
+        'Battleship <x> <y> <direction>',
+        'Cruiser x y direction',
+        'Carrier x y direction',
+        'Destroyer x y direction',
+        'Submarine x y direction'
+    ]
+
+    # Writing the ships location for Phase 1
+    f = open(os.path.join(output_path, command_file), 'w')
+    for ship in ships:
+        f.write(ship + "\n")
+    f.close
+
+def write_shot(x,y,cmd):
+    " Writing command to command_file text"
+    f = open(os.path.join(output_path, command_file), 'w')
+    f.write("%d,%d,%d\n" %(cmd, x, y))
+    f.close
