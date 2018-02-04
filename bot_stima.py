bab3555fc869addd2772e8b1adc0889ab2aa4eda
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2018-02-04 23:23:12 +0700

    Bot initialization
    + added command converter

---
+++ bot_stima.py
@@ -0,0 +1,11 @@
+# Command converter
+commands = {
+    "singleShot" : 1,
+    "doubleShotVer" : 2,
+    "doubleShotHor" : 3,
+    "cornerShot" : 4,
+    "crossDiagonalShot" : 5,
+    "crossHorizontalShot" : 6,
+    "seekerMissile" : 7,
+    "shield" : 8
+}
