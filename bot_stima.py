0ee9eeecf1021af68e151ede3b83c568f2cff436
Shevalda Gracielira <32480435+shevalda@users.noreply.github.com>
2018-02-09 21:08:18 +0700

    More function prototype
    + several functions defined
    + ships location for different sizes have been stated

--- bot_stima.py
+++ bot_stima.py
@@ -1,13 +1,26 @@
 import os
+import json
 
 # File and path names
 command_file = "command.txt"
 place_ship_file = "place.txt"
 game_state_file = "state.json"
-output_path = '.'   # NOT YET SURE
-map_size = 0        # NOT YET SURE
+output_path = '.'
+
+# Other variables used in strategy
+map_size = 0
+player_map = []         # state-state untuk setiap kotak dalam peta player
+player_ships = []       # state-state untuk semua kapal yang dimiliki player
+enemy_ships = []        # nama-nama kapal musuh yang masih hidup
+state = {}              # hasil pembacaan dari file json
 
 # Command converter
+map_size_name = {
+    7 : "small",
+    10 : "medium",
+    14 : "large"
+}
+
 commands = {
     "singleShot" : 1,
     "doubleShotVer" : 2,
@@ -19,7 +32,7 @@ commands = {
     "shield" : 8
 }
 
-ship_size = {
+ship_info = {
     "Battleship" : 4,
     "Cruiser" : 3,
     "Submarine" : 3,
@@ -27,29 +40,26 @@ ship_size = {
     "Carrier" : 5
 }
 
-ships_location = {
-    "small" : {
-        "Battleship" : [],
-        "Cruiser" : [],
-        "Submarine" : [],
-        "Destroyer" : [],
-        "Carrier" : []
-        },
-    "medium" : {
-        "Battleship" : [],
-        "Cruiser" : [],
-        "Submarine" : [],
-        "Destroyer" : [],
-        "Carrier" : []
-        },
-    "large" : {
-        "Battleship" : [],
-        "Cruiser" : [],
-        "Submarine" : [],
-        "Destroyer" : [],
-        "Carrier" : []
-        }     
-    }
+def main(player_key):
+    global map_size, player_map, player_ships, state, enemy_ships
+    
+    # Retrieve current game state
+    json_file = open(os.path.join(output_path, game_state_file), 'r')
+    state = json.load(json_file)
+    json_file.close()
+
+    map_size = state['MapDimension']
+    if state['Phase'] == 1:     # Phase 1
+        placeShips(map_size)
+    else:                       # Phase 2
+        player_map = state['PlayerMap']['Cells']
+        player_ships = state['PlayerMap']['Owner']['Ships']
+
+        x, y, cmd = arrangingAStrategy()
+
+        enemy_ships = [ship['ShipType'] for ship in state['OpponentMap']['Ships'] if not(ship['Destroyed'])]
+
+        writeCommand(x,y,cmd)
 
 def placeShips(ukuran):
     " This is where the ships location determined "
@@ -57,62 +67,73 @@ def placeShips(ukuran):
     # Ship names: Battleship, Cruiser, Carrier, Destroyer, Submarine
     # Directions: north east south west
 
-    if (ukuran == "small"):
+    if (map_size_name[ukuran] == "small"):
         ships = [
-            'Battleship 1 2 south',
-            'Cruiser 2 0 east',
-            'Carrier 5 1 south',
-            'Destroyer 3 4 south',
-            'Submarine 0 6 east'
+            'Battleship 1 2 north',
+            'Cruiser 2 6 east',
+            'Carrier 5 1 north',
+            'Destroyer 3 1 north',
+            'Submarine 0 0 east'
         ]
-    elif (ukuran == "medium"):
-        # ships = [
-        #     'Battleship <x> <y> <direction>',
-        #     'Cruiser x y direction',
-        #     'Carrier x y direction',
-        #     'Destroyer x y direction',
-        #     'Submarine x y direction'
-        # ]
-    elif (ukuran == "large"):
-        # ships = [
-        #     'Battleship <x> <y> <direction>',
-        #     'Cruiser x y direction',
-        #     'Carrier x y direction',
-        #     'Destroyer x y direction',
-        #     'Submarine x y direction'
-        # ]
+    elif (map_size_name[ukuran] == "medium"):
+        ships = [
+            'Battleship 6 2 north',
+            'Cruiser 1 7 east',
+            'Carrier 8 4 north',
+            'Destroyer 4 3 north',
+            'Submarine 1 1 east'
+        ]
+    elif (map_size_name[ukuran] == "large"):
+        ships = [
+            'Battleship 7 5 north',
+            'Cruiser 2 10 east',
+            'Carrier 10 7 east',
+            'Destroyer 5 6 north',
+            'Submarine 3 2 east'
+        ]
 
     # Writing the ships location for Phase 1
-    f = open(os.path.join(output_path, command_file), 'w')
+    f = open(os.path.join(output_path, place_ship_file), 'w')
     for ship in ships:
         f.write(ship + "\n")
     f.close
 
-def writeShot(x,y,cmd):
+    return
+
+def writeCommand(x,y,cmd):
     " Writing command to command_file text"
     f = open(os.path.join(output_path, command_file), 'w')
     f.write("%d,%d,%d\n" %(cmd, x, y))
     f.close
 
 ### PROTOTYPE ###
-found_ship = False          # mengecek apakah kita sudah menemukan sebuah kapal musuh
-first_hit = (-1,-1)         # titik pertama kali menemukan kapal musuh
-last_hit = (-1,-1)          # menyimpan titik terakhir yang ditembak
-hit_orientation = "n"       # menyimpan orientasi tembakan dalam urutan atas, bawah, kanan, kiri
-list_of_enemy_ship = []     # kapal-kapal musuh yang masih ada
+to_be_shot = []
+found_ship = False      # mengecek apakah kita sudah menemukan sebuah kapal musuh
+first_hit = (-1,-1)     # titik pertama kali menemukan kapal musuh
+last_shot = (-1,-1)     # menyimpan titik terakhir yang ditembak
+last_hit_count = 0      # jumlah hit pada ronde sebelumnya sebelumnya
+enemy_ships = []        # kapal-kapal musuh yang masih ada
 
-def isLastShotHit (last_hit, map):
-    """ Mengembalikan nilai boolean jika last hit mengenai kapal.
-        param:
-            last_hit = [tuple] titik terakhir menembak
-            map = [list of char] peta musuh yang diberikan oleh game engine
-        output: boolean
+def getPhase():
     """
-def createListOfShot(ukuran):
+        Menentukan phase apa sekarang
+        output: integer phase sekarang
+    """
+    global state
+    return state['Phase']
+
+def arrangingAStrategy():
+    """
+        Fungsi utama yang menyusun strategi untuk command dan titik apa yang akan diberikan ke game engine
+        output: x, y, command yang akan diberikan
+    """
+    global map_size, player_map, player_ships, state, enemy_ships
+
+def createListOfShot(map_size):
     """
         Membuat list dari titik yang akan ditembak
         param:
-            ukuran = [string] ukuran map ("small", "medium", "large")
+            map_size = [string] ukuran map ("small", "medium", "large")
         output: list of tuple (x,y)
     """
 
@@ -131,6 +152,14 @@ def countEffectiveShots(center, weapon):
             center = [tuple] center point dari tembakan
             weapon = [string] jenis tembakannya
         output:  integer jumlah titik shot yang belum ditembak sebelumnya
+    """
+
+def isLastShotHit (last_hit, map):
+    """ Mengembalikan nilai boolean jika last hit mengenai kapal.
+        param:
+            last_hit = [tuple] titik terakhir menembak
+            map = [list of char] peta musuh yang diberikan oleh game engine
+        output: boolean
     """
 
 def isCrossShotDiagonalAvail (charge, list_of_ships) :
@@ -184,6 +213,99 @@ def isOpponentShielded (point):
         output: boolean dilindungi atau tidaknya titik yang ditembak
     """
     
-def isShieldNeeded()
+def isShieldNeeded():
+    """
+    """
 
+def isPlayerShipAlive(ship_name):
+    """
+        Mencari tahu apakah sebuah kapal masih hidup atau tidak
+        param:
+            ship_name = [string] nama kapal
+        output: boolean apakah kapal masih hidup
+    """
+    global player_ships
 
+    for ship in player_ships:
+        if (ship['ShipType'] == ship_name):
+            return not(ship['Destroyed'])
+
+def isPointAHit(point):
+    """
+        Mengetahui apakah sebuah titik terjadi hit atau tidak setelah sebuah tembakan di titik itu
+        param:
+            point = [tuple] titik yang ingin diketahui status hitnya
+        output: boolean apakah tembakan sebelumnya ada hit atau tidak
+    """
+    global state
+
+    for cell in state['OpponentMap']['Cells']:
+        if(cell['X'] == point[0] and cell['Y'] == point[1]):
+            return cell['Damaged'] and not(cell['Missed'])
+
+def isEnemyPointShielded(point):
+    """
+        Mengetahui apakah sebuah titik sedang di-shield oleh tidak (setelah ditembak ke titik itu)
+        param:
+            point = [tuple] titik yang ingin diketahui status shieldnya
+        output: boolean apakah titik tersebut dishield atau tidak
+    """
+    global state
+
+    for cell in state['OpponentMap']['Cells']:
+        if(cell['X'] == point[0] and cell['Y'] == point[1]):
+            return cell['ShieldHit']
+
+def isPlayerShieldActive():
+    """
+        Mengetahui apakah shield pemain sudah/masih aktif
+        output: boolean yang menyatakan shield pemain masih aktif
+    """
+    global state
+
+    return state['PlayerMap']['Owner']['Shield']['Active']
+
+def getShipWeaponEnergy(ship_name):
+    """
+        Mengembalikan nilai energi yang butuhkan senjata khusus sebuah kapal
+        param:
+            ship_name = [string] nama kapal
+        output: integer nilai energi senjata khusus kapal tersebut
+    """
+    global player_ships
+
+    for ship in player_ships:
+        if (ship['ShipType'] == ship_name):
+            return ship['Weapons'][1]['EnergyRequired']
+
+def getShipCenterPoint(ship_name, list_of_ship):
+    """
+        Mencari titik tengah sebuah kapal
+        param:
+            ship_name = [string] nama kapal
+            list_of_ship = [list of dictionary] state-state kapal yang dimiliki player
+        output: tuple (x,y) titik tengah kapal
+    """
+
+    for ship in list_of_ship:
+        if (ship['ShipType'] == ship_name):
+            ship_center = (len(ship['Cells']) - 1) // 2
+            return (ship['Cells'][ship_center]['X'], ship['Cells'][ship_center]['Y'])
+
+def getShotEnergy():
+    """
+        Mengetahui jumlah energi yang tersisa untuk menembak
+        output: integer jumlah energi
+    """
+    global state
+
+    return state['PlayerMap']['Owner']['Energy']
+
+def getShotsHit():
+    """
+        Mengetahui jumlah shot hit terhadap musuh
+        output: integer jumlah shot hit
+    """
+    global state
+
+    return state['PlayerMap']['Owner']['ShotsFired']
